dcdb5796482f94d16c0f20aa07b35a4238263f80
niloykumardas0 <niloykumardas0@gmail.com>
2026-06-06 00:45:52 +0600

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/ai-draft-announcements.ts';
-import '@/ai/flows/ai-generate-quiz-questions.ts';
-import '@/ai/flows/smart-proctored-exam-flow.ts';
--- src/firebase/index.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export * from './provider';
-export * from './firestore/use-doc';
-export * from './firestore/use-collection';
